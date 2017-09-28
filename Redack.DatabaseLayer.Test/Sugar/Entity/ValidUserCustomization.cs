0540e5ee420470a663edbc0d836ec5bbd5a41afb
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-09-28 14:21:37 +0200

    Update: adding a customization to allow a recursion path with a depth of two.

--- Redack.DatabaseLayer.Test/Sugar/Entity/ValidUserCustomization.cs
+++ Redack.DatabaseLayer.Test/Sugar/Entity/ValidUserCustomization.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Ploeh.AutoFixture;
+using Redack.DatabaseLayer.Test.Sugar.Customization;
 using Redack.DomainLayer.Model;
 
 namespace Redack.DatabaseLayer.Test.Sugar.Entity
@@ -12,6 +13,7 @@ namespace Redack.DatabaseLayer.Test.Sugar.Entity
     {
         public void Customize(IFixture fixture)
         {
+            fixture.Customize(new OmitOnRecursionCustomization(2));
             fixture.Customize(new ValidCredentialCustomization());
         }
     }
