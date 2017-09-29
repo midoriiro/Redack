01587011103b9de5086652c5d6ed8ad3f427aafa
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-09-29 12:28:41 +0200

    Update:  files naming refactored more clearer

---
+++ Redack.DatabaseLayer.Test/Sugar/Specimen/IgnoreVirtualPropertiesSpecimen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Redack.DatabaseLayer.Test.Sugar.Specimen
+{
+    public class IgnoreVirtualPropertiesSpecimen : ISpecimenBuilder
+    {
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            PropertyInfo propertyInfo = request as PropertyInfo;
+
+            if(propertyInfo != null && propertyInfo.GetGetMethod().IsVirtual)
+                return new OmitSpecimen();
+
+            return new NoSpecimen();
+        }
+    }
+}
