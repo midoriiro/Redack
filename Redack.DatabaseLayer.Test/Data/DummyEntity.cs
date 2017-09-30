d31cdf34d5afe834f6dd9dde726ec233540ce949
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-09-30 09:19:57 +0200

    Update: add dummy entity and changing reference in repository test

---
+++ Redack.DatabaseLayer.Test/Data/DummyEntity.cs
@@ -0,0 +1,12 @@
+﻿using Redack.DomainLayer.Model;
+
+namespace Redack.DatabaseLayer.Test.Data
+{
+    public class DummyEntity : Entity
+    {
+        public string Property1 { get; set; }
+
+        // Navigation properties
+        public DummyEntity Property2 { get; set; }
+    }
+}
--- Redack.DatabaseLayer.Test/TestRepository.cs
+++ Redack.DatabaseLayer.Test/TestRepository.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
+﻿using Moq;
 using Ploeh.AutoFixture;
 using Ploeh.AutoFixture.AutoMoq;
 using Ploeh.AutoFixture.Xunit2;
 using Redack.DatabaseLayer.DataAccess;
+using Redack.DatabaseLayer.Test.Data;
 using Redack.DatabaseLayer.Test.Sugar.Customization;
 using Redack.DatabaseLayer.Test.Sugar.Entity;
-using Redack.DomainLayer.Model;
 using Xunit;
 
 namespace Redack.DatabaseLayer.Test
@@ -19,16 +18,15 @@ namespace Redack.DatabaseLayer.Test
             var fixture = new Fixture();
             fixture.Customize(new AutoMoqCustomization());
             fixture.Customize(new EntityCustomization());
-            fixture.Customize(new ValidUserCustomization());
+            fixture.Customize(new OmitOnRecursionCustomization());
 
-            var user = fixture.Create<User>();
-            var context = fixture.Freeze<Mock<RedackDbContext>>();
-            var sut = fixture.Create<Mock<IRepository<User>>>();
+            var obj = fixture.Create<DummyEntity>();
+            var sut = fixture.Create<Mock<IRepository<DummyEntity>>>();
 
-            sut.Object.Insert(user);
+            sut.Object.Insert(obj);
             sut.Object.Commit();
 
-            sut.Verify(e => e.Insert(It.IsAny<User>()), Times.Once);
+            sut.Verify(e => e.Insert(It.IsAny<DummyEntity>()), Times.Once);
             sut.Verify(e => e.Commit(), Times.Once);
         }
     }
