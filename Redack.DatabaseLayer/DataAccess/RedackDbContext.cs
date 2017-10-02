996b75db791ef2a19b6fb71f31d836ab5fbcdd61
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-10-02 14:06:06 +0200

    Fix: infinite recursion

--- Redack.DatabaseLayer/DataAccess/RedackDbContext.cs
+++ Redack.DatabaseLayer/DataAccess/RedackDbContext.cs
@@ -16,17 +16,18 @@ namespace Redack.DatabaseLayer.DataAccess
         public virtual DbSet<Thread> Threads { get; set; }
         public virtual DbSet<User> Users { get; set; }
 
-        public RedackDbContext(string connectionString) : base(connectionString) {}
-        
+        public RedackDbContext(string connectionString) : base(connectionString)
+        {
+        }
 
         public new DbSet<TEntity> Set<TEntity>() where TEntity : Entity
         {
-            return this.Set<TEntity>();
+            return base.Set<TEntity>();
         }
 
         public new DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : Entity
         {
-            return this.Entry<TEntity>(entity);
+            return base.Entry<TEntity>(entity);
         }
 
         public override int SaveChanges()
@@ -37,18 +38,14 @@ namespace Redack.DatabaseLayer.DataAccess
             }
             catch (DbEntityValidationException ex)
             {
-                // Retrieve the error messages as a list of strings.
                 var errorMessages = ex.EntityValidationErrors
                     .SelectMany(x => x.ValidationErrors)
                     .Select(x => x.ErrorMessage);
 
-                // Join the list to a single string.
                 var fullErrorMessage = string.Join("; ", errorMessages);
 
-                // Combine the original exception message with the new one.
                 var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
 
-                // Throw a new DbEntityValidationException with the improved exception message.
                 throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
             }
         }
