e38854fa5dea7b26980db7f2d6fb3701776ab64f
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-10-02 14:15:36 +0200

    Update: change files location and update packages

---
+++ Redack.DatabaseLayer.Test/TestBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Redack.DatabaseLayer.Test.DataAccess;
+
+namespace Redack.DatabaseLayer.Test
+{
+    public class TestBase
+    {
+        public TestBase()
+        {
+            EffortProviderFactory.ResetDb();
+        }
+    }
+}
