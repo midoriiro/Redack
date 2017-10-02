d47909a15322a537227ced6996167b84de1bb5f3
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-10-02 14:13:21 +0200

    Add: Introduce Effort and NMEmory class

---
+++ Redack.DatabaseLayer.Test/DataAccess/EffortProviderFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redack.DatabaseLayer.Test.DataAccess
+{
+    public class EffortProviderFactory : IDbConnectionFactory
+    {
+        private static DbConnection _connection;
+        private static readonly object Lock = new object();
+
+        public static void ResetDb()
+        {
+            lock (Lock)
+            {
+                _connection = null;
+            }
+        }
+
+        public DbConnection CreateConnection(string nameOrConnectionString)
+        {
+            lock (Lock)
+            {
+                if (_connection == null)
+                {
+                    _connection = Effort.DbConnectionFactory.CreateTransient();
+                }
+
+                return _connection;
+            }
+        }
+    }
+}
