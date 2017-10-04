57520ff5d029edd2dcda00d5b2fb85e29cb4ed5f
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-10-04 10:42:13 +0200

    Update: adding in memory database context

--- Redack.DatabaseLayer.Test/Entity/TestUser.cs
+++ Redack.DatabaseLayer.Test/Entity/TestUser.cs
@@ -2,16 +2,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Ploeh.AutoFixture;
 using Ploeh.AutoFixture.Xunit2;
-using Redack.DatabaseLayer.Test.Sugar.Customization;
-using Redack.DatabaseLayer.Test.Sugar.Entity;
+using Redack.Test.Lollipop.Customization;
+using Redack.Test.Lollipop.Entity;
 using Redack.DomainLayer.Model;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Entity.Validation;
 using Castle.Components.DictionaryAdapter.Xml;
 using Castle.DynamicProxy;
-using Ploeh.AutoFixture.AutoMoq;
 using Redack.DatabaseLayer.DataAccess;
+using Redack.Test.Lollipop;
+using Redack.Test.Lollipop.Data;
 using Xunit;
 
 namespace Redack.DatabaseLayer.Test.Entity
@@ -26,7 +27,8 @@ namespace Redack.DatabaseLayer.Test.Entity
 
             var obj = fixture.Create<User>();
 
-            var sut = new Repository<User>();
+            var context = new DummyDbContext();
+            var sut = new Repository<User>(context);
 
             sut.Insert(obj);
             sut.Commit();
@@ -46,9 +48,11 @@ namespace Redack.DatabaseLayer.Test.Entity
                 "Group",
                 "Permissions"
             }));
-                
+
             var obj = fixture.Create<User>();
-            var sut = new Repository<User>();
+
+            var context = new DummyDbContext();
+            var sut = new Repository<User>(context);
 
             sut.Insert(obj);
 
