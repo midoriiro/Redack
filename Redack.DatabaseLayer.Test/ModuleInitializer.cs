6260424ab24de5cb4353ab30628267ab7910588c
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-10-10 09:43:58 +0200

    Update: minor project files configurations changes

--- Redack.DatabaseLayer.Test/ModuleInitializer.cs
+++ Redack.DatabaseLayer.Test/ModuleInitializer.cs
@@ -1,6 +1,12 @@
+// This file can be modified in any way, with two exceptions. 1) The name of
+// this class must be "ModuleInitializer". 2) This class must have a public or
+// internal parameterless "Run" method that returns void. In addition to these
+// modifications, this file may also be moved to any location, as long as it
+// remains a part of its current project.
+
 using Redack.Test.Lollipop.Configuration;
 
-namespace Redack.Test.Lollipop
+namespace Redack.DatabaseLayer.Test
 {
     internal static class ModuleInitializer
     {
