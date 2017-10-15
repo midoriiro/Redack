a61ce898d09edc3cf7363c3c030a1b0a17d3f9c7
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-10-15 20:01:29 +0200

    Update: identity class decoupled

--- Redack.ServiceLayer/Security/JwtTokenizer.cs
+++ Redack.ServiceLayer/Security/JwtTokenizer.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Web;
 using Jose;
+using Newtonsoft.Json;
 using Redack.DatabaseLayer.DataAccess;
 using Redack.DomainLayer.Model;
 
@@ -11,29 +13,41 @@ namespace Redack.ServiceLayer.Security
 {
     public class JwtTokenizer
     {
-        public string Encode(Identity identity, double expiration)
+        public static string Encode(string key, string issuer, double expiration)
         {
             var payload = new Dictionary<string, object>()
             {
-                { "iss", "redack" },
+                { "iss", issuer },
                 { "iat", DateTime.UtcNow },
                 { "exp", DateTime.UtcNow.AddMinutes(expiration)}
             };
 
             return JWT.Encode(
-                payload, 
-                ApiKey.ToBytes(identity.ApiKey.Key), 
+                payload,
+                Convert.FromBase64String(key), 
                 JweAlgorithm.A256GCMKW, 
                 JweEncryption.A256CBC_HS512);
         }
 
-        public string Decode(Identity identity)
+        public static Dictionary<string, object> Decode(string key, string token)
         {
-            return JWT.Decode(
-                identity.Access, 
-                ApiKey.ToBytes(identity.ApiKey.Key), 
+            string json = JWT.Decode(
+                token,
+                Convert.FromBase64String(key), 
                 JweAlgorithm.A256GCMKW, 
                 JweEncryption.A256CBC_HS512);
+
+            return JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+        }
+
+        public static bool IsValid(string key, string issuer, string token)
+        {
+            var untokenized = Decode(key, token);
+
+            DateTime exp = (DateTime)untokenized["exp"];
+            string iss = (string)untokenized["iss"];
+
+            return DateTime.UtcNow <= exp && iss == issuer;
         }
     }
 }
