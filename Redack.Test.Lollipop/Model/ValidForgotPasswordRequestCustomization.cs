fc3ca7c1bb1fa1604b5d665615e40f4d32de922d
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-10-17 10:42:38 +0200

    Add: forgot password request customization

---
+++ Redack.Test.Lollipop/Model/ValidForgotPasswordRequestCustomization.cs
@@ -0,0 +1,18 @@
+﻿using Ploeh.AutoFixture;
+using Redack.ServiceLayer.Models;
+using Redack.Test.Lollipop.Customization;
+using Redack.Test.Lollipop.Entity;
+
+namespace Redack.Test.Lollipop.Model
+{
+    public class ValidForgotPasswordRequestCustomization : ICustomization
+    {
+        public virtual void Customize(IFixture fixture)
+        {
+            fixture.Customize(new EmailAddressCustomization<ForgotPasswordRequest>("Login"));
+            fixture.Customize(new CopyPropertyValueToAnother<ForgotPasswordRequest>(
+                "NewPassword", "NewPasswordConfirm"));
+            fixture.Customize(new ValidClientCustomization());
+        }
+    }
+}
