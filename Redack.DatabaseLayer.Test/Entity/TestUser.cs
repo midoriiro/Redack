3e489717010dc86bb1857fedfcdc2c12e2a639df
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-10-17 10:39:01 +0200

    Update: user unit tests refactored

--- Redack.DatabaseLayer.Test/Entity/TestUser.cs
+++ Redack.DatabaseLayer.Test/Entity/TestUser.cs
@@ -1,56 +1,198 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Ploeh.AutoFixture;
-using Ploeh.AutoFixture.Xunit2;
+﻿using System.Data.Entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Redack.DatabaseLayer.DataAccess;
 using Redack.DomainLayer.Model;
 using Redack.Test.Lollipop;
-using Redack.Test.Lollipop.Customization;
-using Redack.Test.Lollipop.Entity;
 using System.Data.Entity.Validation;
+using System.Linq;
 using Xunit;
+using Thread = System.Threading.Thread;
 
 namespace Redack.DatabaseLayer.Test.Entity
 {
     public class TestUser : TestBase
     {
-        [Theory, AutoData]
-        public void TestInsert_ValidUser_Success()
+        [Fact]
+        public void Valid()
         {
-            var fixture = new Fixture();
-            fixture.Customize(new ValidUserCustomization());
+            var user = this.CreateValidUser();
 
-            var obj = fixture.Create<User>();
-
-            var context = new RedackDbContext();
-            var sut = new Repository<User>(context);
-
-            sut.Insert(obj);
-            sut.Commit();
-
-            Assert.AreEqual(1, sut.GetAll().Count);
-            Assert.AreEqual(obj, sut.GetById(obj.Id));
+            using (var repository = new Repository<User>())
+            {
+                Assert.IsNotNull(repository.GetById(user.Id));
+            }
         }
 
-        [Theory, AutoData]
-        public void TestCreate_ValidUser_Fail()
+        [Fact]
+        public void WithoutAlias()
         {
-            var fixture = new Fixture();
-            fixture.Customize(new OmitOnRecursionCustomization());
-            fixture.Customize(new IgnorePropertiesCustomization(new []
+            var user = this.CreateValidUser(push: false);
+            user.Alias = null;
+
+            using (var repository = new Repository<User>())
             {
-                "Messages",
-                "Group",
-                "Permissions"
-            }));
+                repository.Insert(user);
 
-            var obj = fixture.Create<User>();
+                Assert.ThrowsException<DbEntityValidationException>(() => repository.Commit());
+            }
+        }
 
-            var context = new RedackDbContext();
-            var sut = new Repository<User>(context);
+        [Fact]
+        public void WithoutCredential()
+        {
+            var user = this.CreateValidUser(push: false);
+            user.Credential = null;
 
-            sut.Insert(obj);
+            using (var repository = new Repository<User>())
+            {
+                repository.Insert(user);
 
-            Assert.ThrowsException<DbEntityValidationException>(() => sut.Commit());
+                Assert.ThrowsException<DbEntityValidationException>(() => repository.Commit());
+            }
+        }
+
+        [Fact]
+        public void WithCredentialCascadeDelete()
+        {
+            var credential = this.CreateValidCredential(push: false);
+            var user = this.CreateValidUser(credential);
+
+            using (var repository = new Repository<User>())
+            {
+                repository.Delete(user);
+                repository.Commit();
+
+                Assert.IsNull(repository.GetById(user.Id));
+            }
+
+            using (var repository = new Repository<Credential>())
+            {
+                Assert.IsNull(repository.GetById(credential.Id));
+            }
+        }
+
+        [Fact]
+        public void WithPermissionsCascadeDelete()
+        {
+            var permission1 = this.CreateValidPermission<User>();
+            var permission2 = this.CreateValidPermission<User>();
+            var permission3 = this.CreateValidPermission<User>();
+
+            var user1 = this.CreateValidUser();
+            user1.Permissions.Add(permission1);
+            user1.Permissions.Add(permission2);
+            user1.Permissions.Add(permission3);
+
+            var user2 = this.CreateValidUser();
+            user2.Permissions.Add(permission1);
+            user2.Permissions.Add(permission2);
+            user2.Permissions.Add(permission3);
+
+            using (var repository = new Repository<User>())
+            {
+                repository.Update(user1);
+                repository.Update(user2);
+                repository.Commit();
+
+                repository.Delete(user1);
+                repository.Commit();
+
+                Assert.IsNull(repository.GetById(user1.Id));
+                Assert.IsNotNull(repository.GetById(user2.Id));
+            }
+
+            using (var repository = new Repository<Permission>(this.Context))
+            {
+                Assert.IsFalse(repository.All().Any(e => e.Users.Any(p => p.Id == user1.Id)));
+
+                Assert.IsNotNull(repository.GetById(permission1.Id));
+                Assert.IsNotNull(repository.GetById(permission2.Id));
+                Assert.IsNotNull(repository.GetById(permission3.Id));
+            }
+        }
+
+        [Fact]
+        public void WithIdentitiesCascadeDelete()
+        {
+            var user = this.CreateValidUser();
+            var client1 = this.CreateValidClient();
+            var client2 = this.CreateValidClient();
+
+            var identity1 = this.CreateValidIdentity(user, client1);
+            var identity2 = this.CreateValidIdentity(user, client2);
+
+            using (var repository = new Repository<User>())
+            {
+                repository.Delete(user);
+                repository.Commit();
+
+                Assert.IsNull(repository.GetById(user.Id));
+            }
+
+            using (var repository = new Repository<Identity>())
+            {
+                Assert.IsNull(repository.GetById(identity1.Id));
+                Assert.IsNull(repository.GetById(identity2.Id));
+            }
+
+            using (var repository = new Repository<Client>())
+            {
+                Assert.IsNotNull(repository.GetById(client1.Id));
+                Assert.IsNotNull(repository.GetById(client2.Id));
+            }
+        }
+
+        [Fact]
+        public void WithMessagesCascadeDelete()
+        {
+            var user = this.CreateValidUser();
+
+            var message1 = this.CreateValidMessage(user);
+            var message2 = this.CreateValidMessage(user);
+            var message3 = this.CreateValidMessage(user);
+
+            using (var repository = new Repository<User>())
+            {
+                repository.Update(user);
+                repository.Commit();
+
+                repository.Delete(user);
+                repository.Commit();
+
+                Assert.IsNull(repository.GetById(user.Id));
+            }
+
+            using (var repository = new Repository<Message>())
+            {
+                Assert.IsNotNull(repository.GetById(message1.Id));
+                Assert.IsNotNull(repository.GetById(message2.Id));
+                Assert.IsNotNull(repository.GetById(message3.Id));
+            }
+        }
+
+        [Fact]
+        public void WithGroupCascadeDelete()
+        {
+            var group = this.CreateValidGroup();
+
+            var user = this.CreateValidUser();
+            user.Groups.Add(group);
+
+            using (var repository = new Repository<User>(this.Context))
+            {
+                repository.Update(user);
+                repository.Commit();
+
+                repository.Delete(user);
+                repository.Commit();
+
+                Assert.IsNull(repository.GetById(user.Id));
+            }
+
+            using (var repository = new Repository<Group>())
+            {
+                Assert.IsNotNull(repository.GetById(group.Id));
+            }
         }
     }
 }
