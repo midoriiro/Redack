28d299d4e27b1bc92aca77d0fac4c08b1db7d768
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-10-20 12:36:33 +0200

    Update: add retrieve query filter

--- Redack.Test.Lollipop/Data/DummyEntity.cs
+++ Redack.Test.Lollipop/Data/DummyEntity.cs
@@ -1,4 +1,7 @@
-﻿namespace Redack.Test.Lollipop.Data
+﻿using System.Collections.Generic;
+using Redack.DomainLayer.Filters;
+
+namespace Redack.Test.Lollipop.Data
 {
     public class DummyEntity : DomainLayer.Models.Entity
     {
@@ -7,7 +10,12 @@
         // Navigation properties
         public virtual DummyEntity Property2 { get; set; }
 
-        public override void Delete()
+        public override List<QueryFilter<DomainLayer.Models.Entity>> Retrieve()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override List<DomainLayer.Models.Entity> Delete()
         {
             throw new System.NotImplementedException();
         }
