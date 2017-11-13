64ee5d1e59aa68b3e92b4e8d301f0dd01c21edf0
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-11-13 12:08:07 +0100

    Update: clean using string

--- Redack.Test.Lollipop/Data/DummyEntity.cs
+++ Redack.Test.Lollipop/Data/DummyEntity.cs
@@ -1,23 +1,24 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace Redack.Test.Lollipop.Data
 {
-    public class DummyEntity : DomainLayer.Models.Entity
-    {
-        public string Property1 { get; set; }
+	public class DummyEntity : DomainLayer.Models.Entity
+	{
+		public string Property1 { get; set; }
 
-        // Navigation properties
-        public virtual DummyEntity Property2 { get; set; }
+		// Navigation properties
+		public virtual DummyEntity Property2 { get; set; }
 
-        public override IQueryable<DomainLayer.Models.Entity> Filter(IQueryable<DomainLayer.Models.Entity> query)
-        {
-            throw new System.NotImplementedException();
-        }
+		public override IQueryable<DomainLayer.Models.Entity> Filter(IQueryable<DomainLayer.Models.Entity> query)
+		{
+			throw new System.NotImplementedException();
+		}
 
-        public override List<DomainLayer.Models.Entity> Delete()
-        {
-            throw new System.NotImplementedException();
-        }
-    }
+		public override List<DomainLayer.Models.Entity> Delete()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
 }
