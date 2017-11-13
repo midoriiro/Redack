f0675f6e4a0a5e2c9fe5516964079552841edc22
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-11-13 12:07:35 +0100

    Update: fix duplicate Id when entities was generated

--- Redack.Test.Lollipop/Entities/BaseEntityCustomization.cs
+++ Redack.Test.Lollipop/Entities/BaseEntityCustomization.cs
@@ -1,15 +1,17 @@
-﻿using System.Threading;
+﻿using System.Security.Cryptography;
+using System.Threading;
 using Ploeh.AutoFixture;
+using Redack.Test.Lollipop.Customizations;
 
 namespace Redack.Test.Lollipop.Entities
 {
-    public class BaseEntityCustomization : ICustomization
-    {
-        public virtual void Customize(IFixture fixture)
-        {
-            // TODO: fix this workaround
-
-            Thread.Sleep(25);
-        }
-    }
+	public class BaseEntityCustomization : ICustomization
+	{
+		public virtual void Customize(IFixture fixture)
+		{
+			// TODO: fix this workaround
+			
+			//Thread.Sleep(50);
+		}
+	}
 }
