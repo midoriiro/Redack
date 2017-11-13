5a9de449456272b8c479246c8ca02882f4550ff3
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-11-13 12:02:27 +0100

    Update: set name unique

--- Redack.DomainLayer/Models/Client.cs
+++ Redack.DomainLayer/Models/Client.cs
@@ -1,39 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 
 namespace Redack.DomainLayer.Models
 {
-    public class Client : Entity
-    {
-        [Required]
-        [MaxLength(50, ErrorMessage = "Type less than 50 characters")]
-        [MinLength(5, ErrorMessage = "Type at least 5 characters")]
-        public string Name { get; set; }
+	public class Client : Entity
+	{
+		[Required]
+		[MaxLength(50, ErrorMessage = "Type less than 50 characters")]
+		[MinLength(5, ErrorMessage = "Type at least 5 characters")]
+		[Index(IsUnique = true)]
+		public string Name { get; set; }
 
-        [Required]
-        public bool IsBlocked { get; set; }
+		[Required]
+		public bool IsBlocked { get; set; }
 
-        // Navigation properties
-        [Required]
-        public virtual ApiKey ApiKey { get; set; }
+		// Navigation properties
+		[Required]
+		public virtual ApiKey ApiKey { get; set; }
 
-        public virtual ICollection<Identity> Identities { get; set; }
+		public virtual ICollection<Identity> Identities { get; set; }
 
-        public Client()
-        {
-            this.IsBlocked = false;
-        }
+		public Client()
+		{
+			this.IsBlocked = false;
+		}
 
-        public override IQueryable<Entity> Filter(IQueryable<Entity> query)
-        {
-            throw new NotImplementedException();
-        }
+		public override IQueryable<Entity> Filter(IQueryable<Entity> query)
+		{
+			throw new NotImplementedException();
+		}
 
-        public override List<Entity> Delete()
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public override List<Entity> Delete()
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
