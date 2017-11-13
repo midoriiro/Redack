c627a3132d6b791e674d5dfec3d06364301b5f5d
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-11-13 12:02:56 +0100

    Update: fix json serialization loop reference

--- Redack.ServiceLayer/App_Start/WebApiConfig.cs
+++ Redack.ServiceLayer/App_Start/WebApiConfig.cs
@@ -1,22 +1,30 @@
 ﻿using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Routing;
+using Newtonsoft.Json;
 using Redack.ServiceLayer.App_Start;
 using Redack.ServiceLayer.Filters;
+using Redack.ServiceLayer.Security;
 
 namespace Redack.ServiceLayer
 {
-    public static class WebApiConfig
-    {
-        public static string DefaultRouteName = "redack";
+	public static class WebApiConfig
+	{
+		public static string DefaultRouteName = "redack";
 
-        public static void Register(HttpConfiguration config)
-        {
-            config.MapHttpAttributeRoutes(new InheritedRouteProvider());
+		public static void Register(HttpConfiguration config)
+		{
+			config.Services.Replace(typeof(IExceptionLogger), new UnhandledExceptionLogger());
+			config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+			config.Formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.Objects;
 
-            config.Routes.MapHttpRoute(
-                name: DefaultRouteName,
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
-    }
+			config.MapHttpAttributeRoutes(new InheritedRouteProvider());
+
+			config.Routes.MapHttpRoute(
+				name: DefaultRouteName,
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+		}
+	}
 }
