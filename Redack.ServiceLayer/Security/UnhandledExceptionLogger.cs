343a134919875abfb12a5924c778cce59821835b
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-11-13 12:06:29 +0100

    Add: handle unhandle exception

---
+++ Redack.ServiceLayer/Security/UnhandledExceptionLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.ExceptionHandling;
+
+namespace Redack.ServiceLayer.Security
+{
+	public class UnhandledExceptionLogger : ExceptionLogger
+	{
+		public override void Log(ExceptionLoggerContext context)
+		{
+			throw context.Exception;
+		}
+	}
+}
