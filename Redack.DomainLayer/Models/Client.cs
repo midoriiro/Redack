772be3816c261929791929261ed1ba49d8908c88
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-11-21 17:51:14 +0100

    Update: add client signin support

--- Redack.DomainLayer/Models/Client.cs
+++ Redack.DomainLayer/Models/Client.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace Redack.DomainLayer.Models
 {
@@ -13,6 +15,13 @@ namespace Redack.DomainLayer.Models
 		[MinLength(5, ErrorMessage = "Type at least 5 characters")]
 		[Index(IsUnique = true)]
 		public string Name { get; set; }
+
+		[Required(ErrorMessage = "The passphrase field is required")]
+		[MinLength(15, ErrorMessage = "Type at least 15 characters")]
+		public string PassPhrase { get; set; }
+
+		[Required(ErrorMessage = "The salt field is required")]
+		public string Salt { get; set; }
 
 		[Required]
 		public bool IsBlocked { get; set; }
@@ -25,7 +34,48 @@ namespace Redack.DomainLayer.Models
 
 		public Client()
 		{
-			this.IsBlocked = false;
+			this.IsBlocked = true;
+		}
+
+		public static Client Create(string name, string passphrase, ApiKey apikey)
+		{
+			var client = new Client() {
+				Name = name,
+				PassPhrase = passphrase,
+				ApiKey = apikey
+			};
+
+			client.ToHash();
+
+			return client;
+		}
+
+		public static byte[] CreateRandomSalt()
+		{
+			byte[] salt = new byte[256 / 8];
+
+			RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+			provider.GetBytes(salt);
+
+			return salt;
+		}
+
+		public void ToHash()
+		{
+			byte[] salt = new byte[256 / 8];
+			RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+			provider.GetBytes(salt);
+
+			this.Salt = Convert.ToBase64String(Client.CreateRandomSalt());
+
+			this.PassPhrase = Credential.ToHash(this.PassPhrase, salt);
+		}
+
+		public bool IsValid(string passphrase)
+		{
+			//passphrase = Credential.ToHash(passphrase, Convert.FromBase64String(this.Salt));
+
+			return this.PassPhrase == passphrase;
 		}
 
 		public override IQueryable<Entity> Filter(IQueryable<Entity> query)
--- Redack.ServiceLayer/Controllers/ClientsController.cs
+++ Redack.ServiceLayer/Controllers/ClientsController.cs
@@ -4,11 +4,11 @@ using Redack.DomainLayer.Models;
 using System.Web.Http;
 using Redack.ServiceLayer.Filters;
 using Redack.ServiceLayer.Models.Request;
+using System.Web.Http.Description;
 
 namespace Redack.ServiceLayer.Controllers
 {
-	[RoutePrefix("api/clients")]
-	[JwtAuthorizationFilter]
+	[RoutePrefix("api/clients")]	
 	public class ClientsController : RepositoryApiController<Client>
 	{
 		public override bool IsOwner(int id)
@@ -16,30 +16,49 @@ namespace Redack.ServiceLayer.Controllers
 			throw new NotImplementedException();
 		}
 
+		[HttpPost]
+		[Route("signin")]
+		[ResponseType(typeof(Client))]
+		public virtual async Task<IHttpActionResult> SignIn([FromBody] ClientSignInRequest request)
+		{
+			if (!this.ModelState.IsValid)
+				return this.BadRequest(this.ModelState);
+
+			var client = (Client)request.ToEntity(this.Context);
+
+			if (client == null || client.IsBlocked)
+				return this.Unauthorized();
+
+			return this.Ok(client);
+		}
+
+		[JwtAuthorizationFilter]
 		[JwtAuthorize("Client.Retrieve", "Administrator")]
 		public override async Task<IHttpActionResult> GetAll()
 		{
 			return await base.GetAll();
 		}
 
+		[JwtAuthorizationFilter]
 		[JwtAuthorize("Client.Retrieve", "Administrator")]
 		public override async Task<IHttpActionResult> Get(int id)
 		{
 			return await base.Get(id);
 		}
 
-		[JwtAuthorize("Client.Create", "Administrator")]
 		public override async Task<IHttpActionResult> Post([FromBody] BaseRequest<Client> request)
 		{
 			return await base.Post(request);
 		}
 
+		[JwtAuthorizationFilter]
 		[JwtAuthorize("Client.Update", "Administrator")]
 		public override async Task<IHttpActionResult> Put(int id, [FromBody] BasePutRequest<Client> request)
 		{
 			return await base.Put(id, request);
 		}
 
+		[JwtAuthorizationFilter]
 		[JwtAuthorize("Client.Delete", "Administrator")]
 		public override async Task<IHttpActionResult> Delete(int id)
 		{
