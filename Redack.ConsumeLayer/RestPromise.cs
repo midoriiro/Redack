078aa6aed097d2408a5b6a124970435701b71569
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2017-11-28 22:02:03 +0100

    Add: promise support for RestClient class

---
+++ Redack.ConsumeLayer/RestPromise.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redack.ConsumeLayer
+{
+	class RestPromise
+	{
+		private readonly Task<HttpResponseMessage> _promise;
+		private HttpResponseMessage _response = null;
+
+		public RestPromise(Task<HttpResponseMessage> promise)
+		{
+			this.Done((v) => { v.Content = null; });
+		}
+
+		private HttpResponseMessage GetResponse()
+		{
+			if (this._response == null)
+				this._response = this._promise.Result;
+
+			return this._response;
+		}
+
+		public RestPromise Done(Action<HttpResponseMessage> action)
+		{
+			var response = this.GetResponse();
+
+			if(response.IsSuccessStatusCode)
+				action(response);
+
+			return this;
+		}
+
+		public RestPromise Failed(Action<HttpResponseMessage> action)
+		{
+			var response = this.GetResponse();
+
+			if (!response.IsSuccessStatusCode)
+				action(response);
+
+			return this;
+		}
+	}
+}
